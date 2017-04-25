2a574082e2204a01fb7c43dc06836b6a9401b0b4
Paige Pittman <paigepittman@Paiges-MacBook-Air.local>
2017-04-25 12:20:44 -0700

    https

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 function displayGifs() {
 
       var person = $(this).attr("data-person");
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         person + "&api_key=dc6zaTOxFJmzC&limit=10";
 
       $.ajax({
